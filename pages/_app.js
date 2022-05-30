3a532ad38e96c2efcfd3c97fcf8e6384aac9e391
jungyup <jungyupkim4@gmail.com>
2022-05-29 21:46:19 -0400

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+    return (
+        <Layout>
+            <Component {...pageProps} />
+            <style jsx global>{`
+                a {
+                    color: teal;
+                }
+            `}</style>
+        </Layout>
+    );
+}
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Seo from '../components/Seo';
+
+export default function About() {
+    return (
+        <div>
+            <Seo title="About" />
+            <h2>About us</h2>
+        </div>
+    );
+}
